e5b9d382a1becba650d4589844ba26d947764abd
fhernandez93 <fhdezale93@gmail.com>
2024-08-28 21:19:00 +0200

    Test

--- docs/make.jl
+++ docs/make.jl
@@ -9,5 +9,5 @@ makedocs(sitename="ECA Documentation",pages = [
     ])
 
     deploydocs(;
-    repo="github.com/fhernandez93/ECA_Documentation",
+    repo="github.com/fhernandez93/ECADocumentation",
 )
